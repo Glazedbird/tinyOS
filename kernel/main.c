b3bba9f8cf44347b47cebef78b53aec37e2afe61
glazedbird <1079231369@qq.com>
2026-05-09 20:46:38 +0800

    experiment1:尝试函数间相互调用保存参数

--- kernel/main.c
+++ kernel/main.c
@@ -2,33 +2,43 @@
 #include "log.h"
 #include "types.h"
 
-void task1() {
+const uint32 TASKFINISHED = 1000000;
 
-    uint32 counter1 = 0;
-    while (1)
+
+// 尝试将 task 的变量保存下来，将前一个task的运行参数保存下来
+// 这样可以简单实现
+// 但是如果参数量不对等呢？task的数量远远多于两个呢？
+// 两个 task
+void task1(uint32 counter1, uint32 counter2) {
+
+    // uint32 counter1 = 0;
+    uartwrite("task1 is running!\n");
+    while (counter1 < TASKFINISHED)
     {   
-        uartwrite("task1 is running!\n");
+        counter1 ++;
         if(counter1 == 1000)
-            yield();
+            task2(counter2, counter1);
     }
 
 }
 
-void task2() {
 
-    uint32 counter1 = 0;
-    while (1)
+void task2(uint32 counter1, uint32 counter2) {
+
+    // uint32 counter1 = 0;
+    uartwrite("task2 is running!\n");
+    while (counter1 < TASKFINISHED)
     {   
-        uartwrite("task2 is running!\n");
+        counter1 ++;
         if(counter1 == 1000)
-            yield();
+            task1(counter2, counter1);
     }
 
 }
 
 int main(void) {
 
-
+    task1(0, 0);
 
 
 
