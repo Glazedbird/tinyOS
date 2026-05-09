5dc89210dd8f2c43fb02ac2fa72a75a9b2968c34
glazedbird <1079231369@qq.com>
2026-05-09 20:12:59 +0800

    proc:

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,33 @@
 #include "uart.h"
 #include "log.h"
+#include "types.h"
+
+void task1() {
+
+    uint32 counter1 = 0;
+    while (1)
+    {   
+        uartwrite("task1 is running!\n");
+        if(counter1 == 1000)
+            yield();
+    }
+
+}
+
+void task2() {
+
+    uint32 counter1 = 0;
+    while (1)
+    {   
+        uartwrite("task2 is running!\n");
+        if(counter1 == 1000)
+            yield();
+    }
+
+}
 
 int main(void) {
 
-    LOG("log 1");
-    LOG("log 2");
 
 
 
